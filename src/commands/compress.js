aeefbc363dfb3897dd2bc710ec2f26fceca8b298
HamitMagic <Hamit.Magic@gmail.com>
2025-05-03 17:41:07 +0500

    fix: compress fix deriction

--- src/commands/compress.js
+++ src/commands/compress.js
@@ -1,15 +1,21 @@
 import fs from 'node:fs';
+import fsPromise from 'node:fs/promises';
 import path from 'node:path';
 import { createBrotliCompress, createBrotliDecompress } from 'node:zlib';
 
 export async function handleCompress(command, args, currentDir) {
-  if (!args[0] || !args[1]) {
+  if (!args[0]) {
     console.error('Invalid input');
     return currentDir;
   }
 
   const srcPath = path.resolve(currentDir, args[0]);
   const destDir = path.resolve(currentDir, args[1]);
+  const stat = await fsPromise.stat(destDir);
+  if (!stat.isDirectory()) {
+    console.log('Wrong Directory');
+    return currentDir;
+  }
   const fileName = path.basename(srcPath);
   const destPath = path.join(destDir, command === 'compress' ? `${fileName}.br` : fileName.replace(/\.br$/, ''));
 
